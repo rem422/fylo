759b52e64e24718d72f2eec682e2a2d5ad2ee3fb
Rem Simiyu <84072712+rem422@users.noreply.github.com>
2023-05-17 21:18:09 +0300

    Fylo App

--- src/Components/FooterSection/Footer.jsx
+++ src/Components/FooterSection/Footer.jsx
@@ -1,214 +1,194 @@
-import Logo from '../Logo'
-import { Link } from 'react-scroll'
-import locationImg from '../../images/icon-location.svg'
-import phoneImg from '../../images/icon-phone.svg'
-import emailImg from '../../images/icon-email.svg'
-import { FaFacebookF ,FaTwitter, FaInstagram } from 'react-icons/fa'
-import styled from 'styled-components'
+import Logo from "../Logo/Logo";
+import { Link } from "react-scroll";
+import locationImg from "../../images/icon-location.svg";
+import phoneImg from "../../images/icon-phone.svg";
+import emailImg from "../../images/icon-email.svg";
+import { FaFacebookF, FaTwitter, FaInstagram } from "react-icons/fa";
+import styled from "styled-components";
 
 function Footer() {
-    
-    const getYear = () => {
-        return new Date().getFullYear();
-    }
+  const getYear = () => {
+    return new Date().getFullYear();
+  };
 
   return (
     <FooterSection>
-        <div className="container">
-            <Link to='hero' className='footer_logo'>
-                <Logo />
-            </Link>
-            <div className="footer_wrapper">
-                <div className="footer_info">
-                    <div className="footer_location">
-                        <img src={locationImg} alt="img" className="footer_img"/>
-                        <p className="footer_location-text">
-                            Lorem ipsum dolor sit amet consectetur adipisicing elit. Recusandae ducimus rem, iste, est suscipit veritatis eaque necessitatibus.
-                        </p>
-                    </div>
-                </div>
-                <div className="footer_contacts">
-                    <div className="footer_contact">
-                        <img src={phoneImg} alt="img" className="footer_img"/>
-                        <p>+1-543-123-4567</p>
-                    </div>
-                    <div className="footer_contact">
-                        <img src={emailImg} alt="img" className="footer_img"/>
-                        <p>example@fylo.com</p>
-                    </div>
-                </div>
-                <ul className="footer_links">
-                    <li className="footer_link">
-                        <Link to='/'>About Us</Link>
-                    </li>
-                    <li className="footer_link">
-                        <Link to='/'>Jobs</Link>
-                    </li>
-                    <li className="footer_link">
-                        <Link to='/'>Press</Link>
-                    </li>
-                    <li className="footer_link">
-                        <Link to='/'>Blog</Link>
-                    </li>
-                </ul>
-                <ul className="footer_links">
-                    <li className="footer_link">
-                        <Link to='/'>Contact Us</Link>
-                    </li>
-                    <li className="footer_link">
-                        <Link to='/'>Terms</Link>
-                    </li>
-                    <li className="footer_link">
-                        <Link to='/'>Privacy</Link>
-                    </li>
-                </ul>
-                <div className="footer_socials">
-                    <div className="footer_socials-icon">
-                        <Link to='/'><FaFacebookF /></Link>
-                    </div>
-                    <div className="footer_socials-icon">
-                        <Link to='/'><FaTwitter /></Link>
-                    </div>
-                    <div className="footer_socials-icon">
-                        <Link to='/'><FaInstagram /></Link>
-                    </div>
-                </div>
+      <div className="container">
+        <Link to="hero" className="footer_logo">
+          <Logo />
+        </Link>
+        <div className="footer_wrapper">
+          <div className="footer_info">
+            <div className="footer_location">
+              <img src={locationImg} alt="img" className="footer_img" />
+              <p className="footer_location-text">
+                Lorem ipsum dolor sit amet consectetur adipisicing elit.
+                Recusandae ducimus rem, iste, est suscipit veritatis eaque
+                necessitatibus.
+              </p>
             </div>
-            <p className="footer_copyright">&copy;Copyright {getYear()} Fylo. All rights reserved</p>
+          </div>
+          <div className="footer_contacts">
+            <div className="footer_contact">
+              <img src={phoneImg} alt="img" className="footer_img" />
+              <p>+1-543-123-4567</p>
+            </div>
+            <div className="footer_contact">
+              <img src={emailImg} alt="img" className="footer_img" />
+              <p>example@fylo.com</p>
+            </div>
+          </div>
+          <ul className="footer_links">
+            <li className="footer_link">
+              <Link to="/">About Us</Link>
+            </li>
+            <li className="footer_link">
+              <Link to="/">Jobs</Link>
+            </li>
+            <li className="footer_link">
+              <Link to="/">Press</Link>
+            </li>
+            <li className="footer_link">
+              <Link to="/">Blog</Link>
+            </li>
+          </ul>
+          <ul className="footer_links">
+            <li className="footer_link">
+              <Link to="/">Contact Us</Link>
+            </li>
+            <li className="footer_link">
+              <Link to="/">Terms</Link>
+            </li>
+            <li className="footer_link">
+              <Link to="/">Privacy</Link>
+            </li>
+          </ul>
+          <div className="footer_socials">
+            <div className="footer_socials-icon">
+              <Link to="/">
+                <FaFacebookF />
+              </Link>
+            </div>
+            <div className="footer_socials-icon">
+              <Link to="/">
+                <FaTwitter />
+              </Link>
+            </div>
+            <div className="footer_socials-icon">
+              <Link to="/">
+                <FaInstagram />
+              </Link>
+            </div>
+          </div>
         </div>
+        <p className="footer_copyright">
+          &copy;Copyright {getYear()} Fylo. All rights reserved
+        </p>
+      </div>
     </FooterSection>
-  )
+  );
 }
 
 const FooterSection = styled.footer`
-    background-color: var(--footerBg);
+  background-color: var(--footerBg);
 
-.footer_logo {
+  .footer_logo {
     margin-top: 3rem;
     position: relative;
     top: 130px;
-}
+  }
 
-.footer_wrapper .footer_img {
+  .footer_wrapper .footer_img {
     width: 15px;
     object-fit: contain;
-}
+  }
 
-.footer_wrapper {
+  .footer_wrapper {
     padding: 10rem 0 4rem 0;
     display: grid;
     grid-template-columns: repeat(5, auto);
     grid-gap: 2rem;
-}
+  }
 
-.footer_wrapper .footer_info .footer_location {
+  .footer_wrapper .footer_info .footer_location {
     display: flex;
     gap: 1.2rem;
     align-items: flex-start;
-}
+  }
 
-.footer_wrapper .footer_info .footer_location .footer_location-text {
+  .footer_wrapper .footer_info .footer_location .footer_location-text {
     width: 100%;
     max-width: 400px;
-}
+  }
 
-.footer_wrapper .footer_contacts .footer_contact {
+  .footer_wrapper .footer_contacts .footer_contact {
     display: flex;
     gap: 1.2rem;
     margin-bottom: 1rem;
-}
+  }
 
-.footer_wrapper .footer_links {
+  .footer_wrapper .footer_links {
     display: flex;
     align-items: flex-start;
     justify-content: flex-start;
     flex-direction: column;
-}
+  }
 
-.footer_wrapper .footer_links  .footer_link {
+  .footer_wrapper .footer_links .footer_link {
     color: var(--textColor);
     margin-bottom: 1rem;
     transition: var(--transition);
-}
+  }
 
-.footer_wrapper .footer_links .footer_link:hover a {
+  .footer_wrapper .footer_links .footer_link:hover a {
     color: var(--white);
-}
+  }
 
-.footer_wrapper .footer_socials {
+  .footer_wrapper .footer_socials {
     display: flex;
     align-items: flex-start;
     justify-content: flex-start;
     flex-direction: row;
-}
+  }
 
-.footer_wrapper .footer_socials {
+  .footer_wrapper .footer_socials {
     display: flex;
-}
+  }
 
-.footer_wrapper .footer_socials .footer_socials-icon a {
+  .footer_wrapper .footer_socials .footer_socials-icon a {
     display: inline-flex;
     margin-right: 1rem;
     color: var(--white);
     border: 2px solid var(--white);
-    padding: .5rem;
+    padding: 0.5rem;
     border-radius: 50%;
     transition: var(--transition);
-}
+  }
 
-.footer_wrapper .footer_socials .footer_socials-icon a:hover {
+  .footer_wrapper .footer_socials .footer_socials-icon a:hover {
     color: var(--btnColor);
     border-color: var(--btnColor);
     transition: var(--transition);
-}
+  }
 
-.footer_copyright {
-    font-size: .8rem;
+  .footer_copyright {
+    font-size: 0.8rem;
     padding-bottom: 1rem;
-}
+  }
 
-@media only screen and (max-width:960px) {
-.footer_wrapper {
-    display: grid;
-    grid-template-columns: repeat(4, auto);
-}
-}
+  @media only screen and (max-width: 960px) {
+    .footer_wrapper {
+      display: grid;
+      grid-template-columns: repeat(4, auto);
+    }
+  }
 
+  @media (max-width: 768px) {
+    .footer_wrapper {
+      display: grid;
+      grid-template-columns: 100%;
+    }
+  }
+`;
 
-@media(max-width: 768px) {
-.footer_wrapper {
-    display: grid;
-    grid-template-columns: 100%;
-}
-}
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-`
-
-
-
-
-export default Footer
+export default Footer;
--- src/Components/NavbarSection/Navbar.jsx
+++ src/Components/NavbarSection/Navbar.jsx
@@ -1,20 +1,19 @@
-import { useEffect, useState } from 'react';
-import { Link } from 'react-scroll';
-import styled from 'styled-components';
-import Logo from '../Logo'
-import { FiMenu, FiX } from 'react-icons/fi'
+import { useEffect, useState } from "react";
+import { Link } from "react-scroll";
+import styled from "styled-components";
+import Logo from "../Logo/Logo";
+import { FiMenu, FiX } from "react-icons/fi";
 
 function Navbar() {
-
   const [Navbar, setNavbar] = useState(false);
-// navbar scrollchangeBackground function
-const changeBg = () => {
-  if (window.scrollY > 60) {
-    setNavbar(true)
-  } else {
-    setNavbar(false)
-  }
-};
+  // navbar scrollchangeBackground function
+  const changeBg = () => {
+    if (window.scrollY > 60) {
+      setNavbar(true);
+    } else {
+      setNavbar(false);
+    }
+  };
 
   useEffect(() => {
     changeBg();
@@ -22,173 +21,198 @@ const changeBg = () => {
     window.addEventListener("scroll", changeBg);
   }, []);
 
-// Shows the Menu button when the screen size is equal to or less than 760px
-const [isMobile, setIsMobile] = useState(
-  window.matchMedia('(max-width: 768px)').matches
-);
+  // Shows the Menu button when the screen size is equal to or less than 760px
+  const [isMobile, setIsMobile] = useState(
+    window.matchMedia("(max-width: 768px)").matches
+  );
 
-useEffect(() => {
-  window.addEventListener('resize', () => {
-    setIsMobile(window.matchMedia('(max-width: 768px)').matches)
-  }, []);
-});
+  useEffect(() => {
+    window.addEventListener(
+      "resize",
+      () => {
+        setIsMobile(window.matchMedia("(max-width: 768px)").matches);
+      },
+      []
+    );
+  });
 
-// Shows the Navmenu
-const[isNavOpen, setIsNavOpen] = useState(false);
+  // Shows the Navmenu
+  const [isNavOpen, setIsNavOpen] = useState(false);
 
   return (
-    <NavbarSection className={Navbar ? 'sticky' : undefined}>
-        <div className="container">
-            <div className="nav">
-                <Link to="hero">
-                  <Logo />
-                </Link>
-                <div className="navMenu">
-                  <nav className={isMobile && isNavOpen ? 'open' : undefined}>
-                    {isMobile && (
-                      <div className="close_icon" tabIndex='0' role='button' onClick={() => setIsNavOpen(false)} onKeyDown={() => setIsNavOpen(false)}>
-                        <FiX />
-                      </div>
-                    )}
-                    <ul>
-                      <li>
-                        <Link to='hero' onClick={() => setIsNavOpen(false)}>Home</Link>
-                      </li>
-                      <li>
-                        <Link to='features' onClick={() => setIsNavOpen(false)}>Features</Link>
-                      </li>
-                      <li>
-                        <Link to='team' onClick={() => setIsNavOpen(false)}>Team</Link>
-                      </li>
-                      <li>
-                        <Link to='testimonials' onClick={() => setIsNavOpen(false)}>Testimonials</Link>
-                      </li>
-                      <li>
-                        <Link to='Newsletter' onClick={() => setIsNavOpen(false)}>Sign In</Link>
-                      </li>
-                    </ul>
-                  </nav>
-                  {isMobile && (
-                        <div className="menu_icon" tabIndex="0" role="button" onClick={() => setIsNavOpen(true)}
-                        onKeyDown={() => setIsNavOpen(true)}>
-                            <FiMenu />
-                        </div>
-                    )}
+    <NavbarSection className={Navbar ? "sticky" : undefined}>
+      <div className="container">
+        <div className="nav">
+          <Link to="hero">
+            <Logo />
+          </Link>
+          <div className="navMenu">
+            <nav className={isMobile && isNavOpen ? "open" : undefined}>
+              {isMobile && (
+                <div
+                  className="close_icon"
+                  tabIndex="0"
+                  role="button"
+                  onClick={() => setIsNavOpen(false)}
+                  onKeyDown={() => setIsNavOpen(false)}
+                >
+                  <FiX />
                 </div>
-            </div>
+              )}
+              <ul>
+                <li>
+                  <Link to="hero" onClick={() => setIsNavOpen(false)}>
+                    Home
+                  </Link>
+                </li>
+                <li>
+                  <Link to="features" onClick={() => setIsNavOpen(false)}>
+                    Features
+                  </Link>
+                </li>
+                <li>
+                  <Link to="team" onClick={() => setIsNavOpen(false)}>
+                    Team
+                  </Link>
+                </li>
+                <li>
+                  <Link to="testimonials" onClick={() => setIsNavOpen(false)}>
+                    Testimonials
+                  </Link>
+                </li>
+                <li>
+                  <Link to="Newsletter" onClick={() => setIsNavOpen(false)}>
+                    Sign In
+                  </Link>
+                </li>
+              </ul>
+            </nav>
+            {isMobile && (
+              <div
+                className="menu_icon"
+                tabIndex="0"
+                role="button"
+                onClick={() => setIsNavOpen(true)}
+                onKeyDown={() => setIsNavOpen(true)}
+              >
+                <FiMenu />
+              </div>
+            )}
+          </div>
         </div>
+      </div>
     </NavbarSection>
-  )
+  );
 }
 
 const NavbarSection = styled.div`
-    position: fixed;
-    top: 0;
-    left: 0;
-    width: 100%;
-    height: 70px;
-    background-color: rgba(255, 255, 255, 0.2);
-    backdrop-filter: blur(20px) saturate(160%) contrast(45%) brightness(140%);
-    -webkit-backdrop-filter: blur(20px) saturate(160%) contrast(45%) brightness(140%);
-    background-color: transparent;
-    backdrop-filter: blur(30px);
-    transition: var(--transition);
-    z-index: 999;
+  position: fixed;
+  top: 0;
+  left: 0;
+  width: 100%;
+  height: 70px;
+  background-color: rgba(255, 255, 255, 0.2);
+  backdrop-filter: blur(20px) saturate(160%) contrast(45%) brightness(140%);
+  -webkit-backdrop-filter: blur(20px) saturate(160%) contrast(45%)
+    brightness(140%);
+  background-color: transparent;
+  backdrop-filter: blur(30px);
+  transition: var(--transition);
+  z-index: 999;
 
-    &.sticky {
+  &.sticky {
+    background-color: var(--footerBg);
+  }
+
+  .nav {
+    display: flex;
+    align-items: center;
+    justify-content: space-between;
+    padding: 1rem 0;
+  }
+
+  .navMenu {
+    display: flex;
+    align-items: center;
+    justify-content: flex-end;
+  }
+
+  nav ul li {
+    display: inline-block;
+    margin: 0 0.5rem;
+
+    a {
+      display: inline-block;
+      padding: 0.5rem 1rem;
+      color: var(--textColor);
+      font-weight: 500;
+      transition: var(--transition);
+
+      &:hover {
+        color: var(--white);
+        text-decoration: underline;
+      }
+    }
+  }
+
+  .menu_icon,
+  .close_icon {
+    cursor: pointer;
+    padding: 3px;
+    font-size: 1.4rem;
+    transition: 0.3s ease;
+    svg {
+      color: var(--textColor);
+    }
+  }
+
+  .close_icon {
+    position: absolute;
+    right: 14px;
+    top: 19px;
+    transition: all 0.3s ease;
+    &:hover {
+      svg {
+        color: var(--white);
+      }
+    }
+  }
+
+  /* Media queries */
+  @media (max-width: 768px) {
+    nav {
+      display: flex;
+      flex-direction: column;
+      justify-content: center;
+      position: fixed;
+      width: 90%;
+      max-width: 250px;
+      top: 0;
+      right: 0%;
       background-color: var(--footerBg);
+      height: 100vh;
+      z-index: 100;
+      transform: translateX(100%);
+      transition: transform 0.3s ease;
+      overflow: hidden;
     }
 
-    .nav {
-      display: flex;
-      align-items: center;
-      justify-content: space-between;
-      padding: 1rem 0;
-    }
-
-    .navMenu {
-      display: flex;
-      align-items: center;
-      justify-content: flex-end;
+    nav.open {
+      transform: translateX(0%);
     }
 
     nav ul li {
-      display: inline-block;
-      margin: 0 .5rem;
+      display: block;
+      text-align: right;
+      width: 100%;
+      margin: 0.5rem 0;
 
       a {
-        display: inline-block;
-        padding: .5rem 1rem;
-        color: var(--textColor);
-        font-weight: 500;
-        transition: var(--transition);
-
-        &:hover {
-          color: var(--white);
-          text-decoration: underline;
-        }
+        display: block;
+        width: 100%;
       }
     }
+  }
+`;
 
-        .menu_icon,
-        .close_icon {
-            cursor: pointer;
-            padding: 3px;
-            font-size: 1.4rem;
-            transition: .3s ease;
-            svg {
-                color: var(--textColor);
-            }
-        }
-        
-        .close_icon {
-            position: absolute;
-            right: 14px;
-            top: 19px;
-            transition: all .3s ease;
-            &:hover {
-                svg {
-                    color: var(--white);
-                }
-            }
-        }
-
-         /* Media queries */
-        @media(max-width: 768px) {
-            nav {
-                display: flex;
-                flex-direction: column;
-                justify-content: center;
-                position: fixed;
-                width: 90%;
-                max-width: 250px;
-                top: 0;
-                right: 0%;
-                background-color: var(--footerBg);
-                height: 100vh;
-                z-index: 100;
-                transform: translateX(100%);
-                transition: transform 0.3s ease;
-                overflow: hidden;
-            }
-
-            nav.open {
-                transform: translateX(0%);
-            }
-
-            nav ul li {
-                display: block;
-                text-align: right;
-                width: 100%;
-                margin: .5rem 0;
-
-                a {
-                    display: block;
-                    width: 100%;
-                }
-            }
-        }
-`
-
-
-export default Navbar
+export default Navbar;
